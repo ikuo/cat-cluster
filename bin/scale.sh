98296b52bb7bbafe8743b46303e8d4511793f94f
Ikuo Matsumura <makiczar@gmail.com>
2017-05-07 10:41:44 +0000

    Fixup scale.sh to sensor=8

--- bin/scale.sh
+++ bin/scale.sh
@@ -24,24 +24,26 @@ function wait_until_num_entities {
 }
 
 function lamp {
-  wait_until_num_entities 50000
+  wait_until_num_entities 30000
   ENV=small docker-compose scale cat=5
-  wait_until_num_entities 100000
+  wait_until_num_entities 60000
   ENV=small docker-compose scale cat=10
-  wait_until_num_entities 150000
+  wait_until_num_entities 90000
   ENV=small docker-compose scale cat=15
-  wait_until_num_entities 200000
+  wait_until_num_entities 120000
   ENV=small docker-compose scale sensor=2
-  wait_until_num_entities 400000
+  wait_until_num_entities 200000
   ENV=small docker-compose scale sensor=3
-  wait_until_num_entities 600000
+  wait_until_num_entities 300000
   ENV=small docker-compose scale sensor=4
-  wait_until_num_entities 1000000
+  wait_until_num_entities 400000
   ENV=small docker-compose scale sensor=5
-  wait_until_num_entities 1500000
+  wait_until_num_entities 500000
   ENV=small docker-compose scale sensor=6
-  wait_until_num_entities 2000000
+  wait_until_num_entities 600000
   ENV=small docker-compose scale sensor=7
+  wait_until_num_entities 700000
+  ENV=small docker-compose scale sensor=8
 }
 
 subcommand=$1
