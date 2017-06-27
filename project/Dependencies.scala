5a8fbf832a117ce467da9f4eb84db543dee9de5d
Ikuo Matsumura <makiczar@gmail.com>
2017-06-27 04:56:44 +0000

    Setup dependency

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,5 +4,6 @@ object Dependencies {
   lazy val specs2 = "org.specs2" %% "specs2-core" % "3.8.9"
   lazy val ficus = "com.iheart" %% "ficus" % "1.4.0"
   lazy val akkaRedis = "com.safety-data" %% "akka-persistence-redis" % "0.2.0-SNAPSHOT"
+  lazy val splitBrainResolver = "com.github.TanUkkii007" %% "akka-cluster-custom-downing" % "0.0.7"
   def akka(module: String) = "com.typesafe.akka" %% s"akka-$module" % "2.5.0"
 }
--- project/Resolvers.scala
+++ project/Resolvers.scala
@@ -2,4 +2,5 @@ import sbt._
 
 object Resolvers {
   val ivyLocal = Resolver.file("local-ivy", file(Path.userHome.absolutePath + "/.ivy2/local"))
+  val tanukkii007 = Resolver.bintrayRepo("tanukkii007", "maven")
 }
